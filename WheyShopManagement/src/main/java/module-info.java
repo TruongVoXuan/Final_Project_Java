ae91cc5e5c24cd13789ed5ee3552b776a50438c7
TruongVoXuan <truong.75.cvan@gmail.com>
2024-11-12 14:19:31 +0700

    Create_Folder_Project

---
+++ WheyShopManagement/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module truongvx.wheyshopmanagement {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens truongvx.wheyshopmanagement to javafx.fxml;
+    exports truongvx.wheyshopmanagement;
+}
---
+++ WheyShopManagement/src/main/java/truongvx/wheyshopmanagement/HelloController.java
@@ -0,0 +1,14 @@
+package truongvx.wheyshopmanagement;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
