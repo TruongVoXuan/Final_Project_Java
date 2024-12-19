ba1efc2de8958075b79435d6fd49031aa28335a4
TruongVoXuan <truong.75.cvan@gmail.com>
2024-12-19 22:07:58 +0700

    Thống_kê_DASHBOARD

--- WheyShopManagement/src/main/java/truongvx/wheyshopmanagement/controllers/mainFormController.java
+++ WheyShopManagement/src/main/java/truongvx/wheyshopmanagement/controllers/mainFormController.java
@@ -9,6 +9,8 @@ import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.AreaChart;
+import javafx.scene.chart.BarChart;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
@@ -179,6 +181,26 @@ public class mainFormController  implements Initializable {
   private TableView<customersData> customers_tableView;
 
 
+  @FXML
+  private BarChart<?, ?> dashboard_CustomerChart;
+
+  @FXML
+  private Label dashboard_NC;
+
+  @FXML
+  private Label dashboard_NSP;
+
+  @FXML
+  private Label dashboard_TI;
+
+  @FXML
+  private Label dashboard_TotalI;
+
+  @FXML
+  private AreaChart<?, ?> dashboard_incomeChart;
+
+
+
   private Alert alert;
 
   private Connection connect;
@@ -189,8 +211,92 @@ public class mainFormController  implements Initializable {
   private Image image;
 
 
+
   private  ObservableList<productData> cardListData = FXCollections.observableArrayList();
 
+
+  public  void dashboardDisplayNC() {
+    String sql = "SELECT COUNT(id) FROM receipt";
+
+    connect = database.connectDB();
+
+    try{
+        int nc =0;
+      prepare = connect.prepareStatement (sql);
+      result = prepare.executeQuery () ;
+
+      if(result.next())
+      {
+        nc = result.getInt("COUNT(id)");
+      }
+      dashboard_NC.setText(String.valueOf(nc));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void dashboardDisplayTI() {
+    Date date = new Date();
+    java.sql.Date sqlDate = new java.sql.Date(date.getTime());
+    String sql = "SELECT SUM(total) FROM receipt WHERE date = '" + sqlDate + "'";
+
+    connect = database.connectDB();
+    try {
+      double ti = 0;
+      prepare = connect.prepareStatement(sql);
+      result = prepare.executeQuery();
+
+      if (result.next()) {
+        ti = result.getDouble(1); // Sử dụng chỉ số cột thay vì tên cột
+      }
+      System.out.println("Today's income: $" + ti); // In ra giá trị để kiểm tra
+      dashboard_TI.setText("$" + ti);
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void dashboardTotalI() {
+    String sql = "SELECT SUM(total) FROM receipt";
+
+    connect = database.connectDB();
+    try {
+      float ti = 0;
+      prepare = connect.prepareStatement(sql);
+      result = prepare.executeQuery();
+
+      if (result.next()) {
+        ti = result.getFloat(1); // Sử dụng chỉ số cột thay vì tên cột
+      }
+      System.out.println("Total income: $" + ti); // In ra giá trị để kiểm tra
+      dashboard_TotalI.setText("$" + ti);
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void dashboardNSP () {
+    String sql = "SELECT COUNT(quantity) FROM customer";
+
+    connect = database.connectDB();
+    try {
+
+      int q = 0;
+      prepare = connect.prepareStatement (sql);
+      result = prepare.executeQuery () ;
+
+      if(result.next () ) {
+        q = result.getInt("COUNT(quantity)");
+      }
+      dashboard_NSP.setText(String.valueOf(q));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  
 
   public  void inventoryAddBtn() {
       if(inventory_productID.getText().isEmpty() ||  inventory_productName.getText().isEmpty() || inventory_type.getSelectionModel().getSelectedItem()==null || inventory_stock.getText().isEmpty() || inventory_price.getText().isEmpty() || inventory_status.getSelectionModel().getSelectedItem()==null || data.path == null){
@@ -831,6 +937,11 @@ public class mainFormController  implements Initializable {
       menu_form.setVisible(false);
       customers_form.setVisible(false);
 
+      dashboardDisplayNC();
+      dashboardDisplayTI();
+      dashboardTotalI();
+      dashboardNSP();
+
     }
     else  if (event.getSource() == inventory_btn){
       dashboard_form.setVisible(false);
@@ -944,6 +1055,12 @@ public class mainFormController  implements Initializable {
   public void initialize(URL url, ResourceBundle resourceBundle) {
 
     displayUsername();
+
+    dashboardDisplayNC();
+    dashboardDisplayTI();
+    dashboardTotalI();
+    dashboardNSP();
+
     inventoryTypeList();
     inventoryStatusList();
     inventoryShowData();
